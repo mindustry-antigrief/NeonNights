80a7da7b1a555c1da8f8541f50c60a4a49c0234b
lwang393 <68400583+genNAowl@users.noreply.github.com>
2021-10-17 18:24:39 -0700

    Scripts

---
+++ scripts/graphics/blocks.js
@@ -0,0 +1,6 @@
+Blocks.mender.baseColor = Pal.heal
+Blocks.mender.phaseColor = Color.valueOf("ffd59e")
+Blocks.mendProjector.baseColor = Pal.heal
+Blocks.mendProjector.phaseColor = Color.valueOf("ffd59e")
+Blocks.repairPoint.laserColor = Pal.heal
+Blocks.repairTurret.laserColor = Pal.heal
