da4f8f66839784c8cbafffed0e649133c73d3dbf
lwang393 <68400583+genNAowl@users.noreply.github.com>
2021-10-20 10:55:16 -0700

    Cooler Menders

--- scripts/graphics/pal.js
+++ scripts/graphics/pal.js
@@ -1,4 +1,4 @@
-Pal.heal = Color.valueOf("6cf5d7")
-Pal.sap = Color.valueOf("814c9e")
-Pal.sapBullet = Color.valueOf("f17afa")
-Pal.sapBulletBack = Color.valueOf("7343bf")
+Pal.heal.set(0.424, 0.961, 0.843)
+Pal.sap.set(0.506, 0.298, 0.620)
+Pal.sapBullet.set(0.945, 0.478, 0.980)
+Pal.sapBulletBack.set(0.451, 0.263, 0.749)
